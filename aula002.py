935e2b46319b7f240e44a22fc4b999419a27b6bb
Pedro Ramos <pedrorcabeleira2@gmail.com>
2023-10-09 16:28:04 -0300

    3 primeiras aulas

---
+++ aula002.py
@@ -0,0 +1,7 @@
+nome_personagem = 'Pedro'
+idade_personagem = '70'
+
+print('tenho um colega chamado ' + nome_personagem )
+print('ele tem ' + idade_personagem + ' anos')
+print('ele gosta de se chamar ' + nome_personagem)
+print('mas não gosta de ter ' + idade_personagem + ' anos')
