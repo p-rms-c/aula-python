398683a8080c10480c13c6e125e872f8bbf1a5db
Pedro Ramos <pedrorcabeleira2@gmail.com>
2023-10-10 13:47:22 -0300

    aulas 5 e 6

---
+++ aula005.py
@@ -0,0 +1,15 @@
+nome = 'Pedro Ramos'
+adjetivo = ' é foda'
+nome = nome.replace('Ramos', 'Ramos Cabeleira')
+
+print(nome.lower() +  adjetivo.upper())
+
+print(adjetivo.islower())
+
+print(nome[3:10])
+print('/')
+
+print(nome.index('o'))
+print('/')
+
+print(nome)
---
+++ aula006.py
@@ -0,0 +1,39 @@
+# Aritmetica Basica
+from math import *
+
+
+numero = 22
+
+print(numero - numero)
+print('/')
+
+print(10 % 3)
+print('/')
+
+print(str(numero) + " mil anos depois")
+print('/')
+
+print(abs(numero))
+print('/')
+
+print(pow(numero, 2))
+print('/')
+
+print(max(5, 2))
+print('/')
+
+print(min(5, 2))
+print('/')
+
+print(floor(7.9))
+print('/')
+
+print(ceil(7.2))
+print('/')
+
+print(round(77.8))
+print('/')
+
+print(sqrt(144))
+print('/')
+
