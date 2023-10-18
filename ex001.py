d393e7b36fb957e039447aba8fb40a89bc9f63c0
Pedro Ramos <pedrorcabeleira2@gmail.com>
2023-10-18 16:53:50 -0300

    ex001 com while

--- ex001.py
+++ ex001.py
@@ -1,13 +1,17 @@
+
+
 def verifica_cpf():
-    cpf_usuario = input('Digite seu cpf \n')
-        
+    situacao = False
+    while situacao == False:
+        cpf_usuario = input('Digite seu cpf \n')
+        x = cpf_usuario.split('.')
+        y = cpf_usuario.split('-')
 
-    x = cpf_usuario.split('.')
-    y = cpf_usuario.split('-')
+        if len(cpf_usuario) == 14 and len(x) == 3 and len(y) == 2:
+            print('cpf válido')
+            situacao = True
+        else: 
+            print('cpf inválido')
 
-    if len(cpf_usuario) == 14 and len(x) == 3 and len(y) == 2:
-        print('cpf válido')
-    else: 
-        print('cpf inválido')
 
 verifica_cpf()
